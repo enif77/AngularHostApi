716ab8707acc89f6571e1da9c3f6254181db061f
Přemysl Fára <enif@email.cz>
2021-12-30 13:00:12 +0100

    The HTTP server runs in a background task.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-//var builder = WebApplication.CreateBuilder(args);
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
     Args = args,
@@ -10,6 +9,7 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,12 +29,60 @@ app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
+
 // https://github.com/dotnet/aspnetcore/blob/main/src/DefaultBuilder/src/WebApplication.cs
 // https://github.com/aspnet/Hosting/blob/master/src/Microsoft.Extensions.Hosting.Abstractions/HostingAbstractionsHostExtensions.cs
-app.Run();
+//app.Run();
+
+
+// https://docs.microsoft.com/cs-cz/dotnet/api/system.threading.tasks.task.run?view=net-6.0
+var tokenSource = new CancellationTokenSource();
+var token = tokenSource.Token;
+
+var t = Task.Run(async () => 
+{
+    // if (token.IsCancellationRequested)
+    // {
+    //     token.ThrowIfCancellationRequested();
+    // }
+
+    Console.WriteLine("Starting the app server...");
+    await app.RunAsync(token);
+    Console.WriteLine("The app server stopped.");
+}, token);
+
+await Task.Yield();
+
+Console.WriteLine("Running...");
+
+_ = Console.ReadLine();  // This represents a running application (the launcher).
+
+Console.WriteLine("Stopping...");
+tokenSource.Cancel();
+
+try
+{
+    await t;
+    
+    Console.WriteLine("Stopped.");
+}
+catch (AggregateException e)
+{
+    Console.WriteLine("Exception messages:");
+    foreach (var ie in e.InnerExceptions)
+    {
+        Console.WriteLine("   {0}: {1}", ie.GetType().Name, ie.Message);
+    }
+    
+    Console.WriteLine("\nTask status: {0}", t.Status);       
+}
+finally
+{
+    tokenSource.Dispose();
+}
 
 Console.WriteLine("App: DONE!");
