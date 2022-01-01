b907234c7ab8297e71c2974b0ea9108ed50fb1b8
Přemysl Fára <enif@email.cz>
2022-01-01 09:43:13 +0100

    TestController in the AppServer project.

---
+++ AngularHostApi.AppServer/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+/* AngularHostApi - (C) 2021 Premysl Fara  */
+
+namespace AngularHostApi.AppServer.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    
+    using Microsoft.Extensions.Logging;
+    
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        [HttpGet(Name = "GetSomething")]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInformation("Something returned");
+            
+            return Summaries.ToArray();
+        }
+    }    
+}
